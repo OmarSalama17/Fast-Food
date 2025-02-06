d1166b9ea0cedfcb339a7fe29379c153ca93c19c
Omarsalama17 <omar.salama.abdallah@gmail.com>
2025-02-06 20:00:47 +0200

    edit-NavBar

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,35 +1,46 @@
-"use client"
+"use client";
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState } from "react";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+    <header className="bg-gray-800 p-4 absolute top-0 left-0 w-full z-50">
+      <div className="container mx-auto flex justify-between items-center relative z-50">
+        <a href="#" className="text-white text-xl font-bold">Navbar Offcanvas</a>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="text-white focus:outline-none"
+        >
+          ☰
+        </button>
+      </div>
+    </header>
+      {isOpen && <div className="fixed inset-0 bg-black opacity-50 z-40" onClick={() => setIsOpen(false)}></div>}
+      <div
+        className={`fixed top-0 left-0 h-full w-72 bg-white shadow-lg transition-transform transform z-40 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
+      >
+        <div className="p-5 relative">
+          <button
+            onClick={() => setIsOpen(false)}
+            className="absolute top-4 right-4 text-gray-700 text-xl focus:outline-none"
+          >
+            ✕
+          </button>
+          <h2 className="mt-4 text-lg font-bold">Offcanvas</h2>
+          <ul className="mt-4 space-y-2">
+            <li><a href="#" className="block text-gray-700">Party Order</a></li>
+            <li><a href="#" className="block text-gray-700">Store Locations</a></li>
+            <li><a href="#" className="block text-gray-700">Track Order</a></li>
+            <li><a href="#" className="block text-gray-700">Order History</a></li>
+            <li><a href="#" className="block text-gray-700">Offers</a></li>
+            <li><a href="#" className="block text-gray-700">Explore Menu</a></li>
+          </ul>
+        </div>
+      </div>
+      </>
   );
 }
 
