f98d4722c6fc8f32c2dc5e46a978b1b436ad81d0
Omar Salama <o.salama.abdallah@gmail.com>
2025-07-24 00:38:41 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import Section from "./components/section/Section.js";
 import CategoryCarousel from "./components/ExploreMenu/ExploreMenu.js";
 export const revalidate = 60;
 export default async function Home() {
-                const res = await fetch('https://strapi-fast-food-2.onrender.com/api/products?filters[isOffer][$eq]=true&populate=*',{
+                const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products?filters[isOffer][$eq]=true&populate=*`,{
             next: { revalidate: 60 },
         });
         const dataFilter = await res.json();
