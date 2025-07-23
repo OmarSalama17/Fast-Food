9d541c3dffb91857c1cc800958ea56c279d82c1b
Omar Salama <o.salama.abdallah@gmail.com>
2025-07-24 00:17:57 +0200

    Update axiosClient.js

--- app/_utils/axiosClient.js
+++ app/_utils/axiosClient.js
@@ -4,7 +4,7 @@ const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
 const apiURL = 'https://strapi-fast-food-2.onrender.com/api';
 
 const axiosClient = axios.create({
-    baseURL: apiURL,
+    baseURL: apiKey,
     headers: {
         Authorization:`Bearer ${apiKey}`
     }
